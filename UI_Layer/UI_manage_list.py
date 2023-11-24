5612e84877eb8ea978a28009605c35574f45e5e0
JJBZZRD <buzzard.jj@gmail.com>
2023-11-24 16:11:45 +0000

    fixed duplicate header issues in UI_manager

--- UI_Layer/UI_manage_list.py
+++ UI_Layer/UI_manage_list.py
@@ -25,36 +25,40 @@ class ManageList(tk.Frame):
 
     def create_title(self):
         #this method creates the title and add entry button for each list
-        page_top_frame = tk.Frame(self.root)
-        page_top_frame.pack(side='top')
+        #page_top_frame = tk.Frame(self.root)
+        #page_top_frame.pack(side='top')
 
-        list_title = ttk.Label(page_top_frame, text=self.list_type[0], font=("Helvetica", 20, "bold"))
-        list_title.pack(padx=10, pady=5)
+        list_title = ttk.Label(self, text=self.list_type[0], font=("Helvetica", 20, "bold"))
+        list_title.grid(column=6, row=0, padx=10, pady=5)
 
-        new_plan_button = ttk.Button(page_top_frame, text=self.list_type[1])
-        new_plan_button.pack(padx=10, pady=5)
+        new_plan_button = ttk.Button(self, text=self.list_type[1])
+        new_plan_button.grid(column=6, row=1,padx=10, pady=5)
 
 
 
     def create_search(self):
         #this creates the search bar and results box in a grid, i might seperate these later
-        search_frame = tk.Frame(self.root)
-        search_frame.pack()
+        #search_frame = tk.Frame(self.root)
+        #search_frame.pack()
 
-        search_filters = ttk.Combobox(search_frame, values=self.result_headers, state="readonly")
+        search_filters = ttk.Combobox(self, values=self.result_headers, state="readonly")
         search_filters.set("Filter")  # set default value
-        search_filters.grid(column=5, row=0, padx=5)
+        search_filters.grid(column=5, row=2, padx=5)
         # search_filters.bind('<<ComboboxSelected>>', )
 
-        search_bar = ttk.Entry(search_frame, width=100)
-        search_bar.grid(column=6, row=0, padx=5)
+        search_bar = ttk.Entry(self, width=100)
+        search_bar.grid(column=6, row=2, padx=5)
         # search_bar.pack(padx=10, pady=5)
 
-        search_button = ttk.Button(search_frame, text='Search', command=self.result_list)
-        search_button.grid(column=7, row=0)
+        search_button = ttk.Button(self, text='Search', command=self.result_list)
+        search_button.grid(column=7, row=2)
         # search_button.pack(side='right', padx=10, pady=5)
 
-        results_list = ttk.Treeview(search_frame, columns=self.result_headers, show='headings')
+        # we need a list of lists to represent the data to occupy the results list.
+        # then each list within thi list will populate the row from the treeview widget under the appropriate header
+
+    def create_results(self):
+        results_list = ttk.Treeview(self, columns=self.result_headers, show='headings')
 
         # this lets us change the header values depending on what are being passed
         for i in self.result_headers:
@@ -66,19 +70,40 @@ class ManageList(tk.Frame):
 
         results_list.bind('<Double-1>', self.switch_screen)
 
-        results_list.grid(column=0, row=1, columnspan=13)
-        # we need a list of lists to represent the data to occupy the results list.
-        # then each list within thi list will populate the row from the treeview widget under the appropriate header
+        results_list.grid(column=0, row=3, columnspan=13)
 
     def switch_screen(self, event=None):
-        self.pack_forget()
         self.show_screen(AdminDashboard)
+        self.destroy()
 
 
 class PlanList(ManageList):
 
     def setup_list(self):
         self.list_type = ['Manage Plans', 'Add New Plan']
+        self.result_headers = ['Plan ID', 'Plan Name', 'Plan Type', 'Region', 'Description', 'Start Date', 'End Date']
+        self.results = [['1', 'Austerity relief', 'economic collapse', 'United Kingdom',
+                         'Aims to provide support to those suffering from cosy livs', '25/11/2023', 'next GE']]
+        self.create_title()
+        self.create_search()
+        self.create_results()
+
+
+class VolunteerList(ManageList):
+
+    def setup_list(self):
+        self.list_type = ['Manage Volunteers', 'Add New Volunteer']
+        self.result_headers = ['Plan ID', 'Plan Name', 'Plan Type', 'Region', 'Description', 'Start Date', 'End Date']
+        self.results = [['1', 'Austerity relief', 'economic collapse', 'United Kingdom',
+                         'Aims to provide support to those suffering from cosy livs', '25/11/2023', 'next GE']]
+        self.create_title()
+        self.create_search()
+
+
+class RefugeeList(ManageList):
+
+    def setup_list(self):
+        self.list_type = ['Manage Refugees', 'Add New Refugee']
         self.result_headers = ['Plan ID', 'Plan Name', 'Plan Type', 'Region', 'Description', 'Start Date', 'End Date']
         self.results = [['1', 'Austerity relief', 'economic collapse', 'United Kingdom',
                          'Aims to provide support to those suffering from cosy livs', '25/11/2023', 'next GE']]
--- UI_Layer/UI_manager.py
+++ UI_Layer/UI_manager.py
@@ -6,13 +6,20 @@ class UIManager:
         self.root = root
         self.current_screen = None
         self.page_history = []
+        self.header = None
 
     def show_screen(self, screen_class, *args):
         self.clear_screen()
 
-        # here i have integrated the header to exist at the top of any loaded frames
-        UIHeader.create_header(self.root).pack(side='top', fill='x')
+        if not self.header:
+            self.header = UIHeader.create_header(self.root)
+            self.header.pack(side='top', fill='x')
+        else:
+            # we can update the header with new information here
+            pass
 
+        # here i have integrated the header to exist at the top of any loaded frames
+        #UIHeader.create_header(self.root).pack(side='top', fill='x')
         self.current_screen = screen_class(self.root, self.show_screen, *args)
         self.current_screen.pack(expand=True, fill='both')
         self.page_history.append(screen_class)
