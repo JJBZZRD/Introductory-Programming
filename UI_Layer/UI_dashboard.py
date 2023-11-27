9a199a11d9cc001f362b26f31fd4bb40111b29b1
lukejlatham <141853040+lukejlatham@users.noreply.github.com>
2023-11-27 20:59:16 +0000

    EDITED: dashboard to add edit buttons etc

--- UI_Layer/UI_dashboard.py
+++ UI_Layer/UI_dashboard.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
+from UI_modify_entries import EditCamp
+
 
 
 class Dashboard(tk.Frame):
-    def __init__(self, root, show_screen,*args, **kwargs):
+    def __init__(self, root, show_screen, *args, **kwargs):
         super().__init__(root, **kwargs)
         self.root = root
         self.setup_dashboard()
@@ -13,80 +15,114 @@ class Dashboard(tk.Frame):
         raise NotImplementedError("Subclasses should implement this method to setup the dashboard layout")
 
     def populate_overview_tab(self, tab):
-        # Create a frame for the additional resources
-        additional_resources_frame = tk.Frame(tab, bg='lightgray', bd=2, relief='groove')
-        additional_resources_frame.pack(side='right', fill='y', padx=(5, 0))
-
-        # Add a label for the additional resources section
-        tk.Label(additional_resources_frame, text="Additional Resources Available", bg='lightgray').pack(pady=10)
-
-        # This is a placeholder for actual data and widgets for additional resources
-        tk.Label(additional_resources_frame, text="[Data]", bg='lightgray').pack()
-
-        # Create a canvas for the bar charts in the overview
-        canvas = tk.Canvas(tab, bg='white')
-        canvas.pack(expand=True, fill='both', side='left')
-
-        # Sample data for the bar chart
-        resources = [('Tents', 80), ('Food', 60), ('Water', 50), ('Medicine', 30)]
-
-        # Draw the bar chart
-        for i, (resource, percent) in enumerate(resources):
-            self.draw_bar(canvas, i, resource, percent)
-
-    def populate_camp_tab(self, tab, camp_number):
-        # Create the canvas for drawing resource bars
-        canvas = tk.Canvas(tab, bg='white')
-        canvas.pack(side='left', expand=True, fill='both')
-
-        # Sample data for resources in a specific camp
-        resources = {
-            'Tents': {'total': 10, 'allocated': 4},
-            'Food': {'total': 10, 'allocated': 7},
-            'Water': {'total': 10, 'allocated': 6},
-            'Medicine': {'total': 10, 'allocated': 6}
+        # Sample data for camps and resources
+        camps_data = {
+            'Camp 1': {'Tents': [80, 100], 'Food': [60, 80], 'Water': [50, 70], 'Medicine': [30, 50]},
+            'Camp 2': {'Tents': [40, 60], 'Food': [90, 120], 'Water': [70, 90], 'Medicine': [20, 40]},
+            'Camp 3': {'Tents': [50, 80], 'Food': [30, 60], 'Water': [80, 100], 'Medicine': [60, 80]}
         }
 
-        # Create a frame for the resource indicators
-        resource_frame = tk.Frame(tab, bg='white')
-        resource_frame.pack(side='left', fill='y')
+        # Additional resources
+        additional_resources = {'Tents': [20, 50], 'Food': [40, 100], 'Water': [30, 80], 'Medicine': [10, 60]}
 
-        for resource, counts in resources.items():
-            self.create_resource_indicator(resource_frame, resource, counts['allocated'], counts['total'])
+        camps_frame = tk.Frame(tab)
+        camps_frame.pack(side='left', fill='both', expand=True)
 
-    def create_resource_indicator(self, root, resource, allocated, total):
-        # This creates each resource indicator bar along with +/- buttons
-        frame = tk.Frame(root, bg='white')
-        frame.pack(pady=5, fill='x', padx=10)
+        for camp_name, resources in camps_data.items():
+            camp_frame = tk.Frame(camps_frame, borderwidth=1, relief='solid')
+            camp_frame.pack(side='left', fill='both', expand=True, padx=10, pady=10)
+            tk.Label(camp_frame, text=camp_name, font=('Arial', 16, 'bold')).pack(pady=(5, 10))
 
-        # Resource icon (just a placeholder label here)
-        icon_label = tk.Label(frame, text=f'Icon {resource}', bg='white')
-        icon_label.pack(side='left')
+            for resource_name, (amount, capacity) in resources.items():
+                self.create_resource_frame(camp_frame, resource_name, amount, capacity, camp_name)
 
-        # Bar chart for the resource
-        bar = ttk.Progressbar(frame, orient='horizontal', length=100, mode='determinate')
-        bar['value'] = (allocated / total) * 100
-        bar.pack(side='left', padx=(5, 0))
+            ttk.Button(camp_frame, text="Edit Camp", command=lambda cn=camp_name: self.show_screen(EditCamp, cn)).pack(pady=5)
 
-        # Add buttons for adjusting resource allocation
-        add_button = tk.Button(frame, text='+', command=lambda: self.adjust_resource(resource, 1))
-        sub_button = tk.Button(frame, text='-', command=lambda: self.adjust_resource(resource, -1))
-        add_button.pack(side='left', padx=2)
-        sub_button.pack(side='left', padx=2)
+        additional_resources_frame = tk.Frame(tab, bg='lightgray', bd=2, relief='groove')
+        additional_resources_frame.pack(side='right', fill='y', padx=(5, 0))
+        tk.Label(additional_resources_frame, text="Additional Resources Available", bg='lightgray').pack(pady=10)
+    
+        for resource_name, (amount, capacity) in additional_resources.items():
+            self.create_resource_frame(additional_resources_frame, resource_name, amount, capacity)
+    
+    def populate_camp_tab(self, tab, camp_number):
+        # Split the frame into left and right sections
+        left_frame = tk.Frame(tab, bg='white')
+        right_frame = tk.Frame(tab, bg='white')
+        left_frame.pack(side='left', fill='both', expand=True)
+        right_frame.pack(side='right', fill='both', expand=True)
 
-    def draw_bar(self, canvas, index, resource, percent):
-        # Draw a single bar in the canvas for a given resource
-        x0 = 50 + index * 100  # x-coordinate for the bar
-        y0 = 150  # y-coordinate for the bottom of the bar
-        x1 = x0 + 20  # width of the bar
-        y1 = y0 - percent  # height of the bar (based on percentage)
-        canvas.create_rectangle(x0, y0, x1, y1, fill='gray', outline='black')
-        # Label for the resource
-        canvas.create_text(x0 + 10, y0 + 10, text=f'{resource}', anchor='n')
+        # Example data structure for camp resources
+        camp_resources = {
+            'Tents': [4, 10],
+            'Food': [7, 10],
+            'Water': [6, 10],
+            'Medicine': [6, 10]
+        }
 
-    def adjust_resource(self, resource, adjustment):
-        # Placeholder method to adjust resources
-        pass
+        # Create resources section in left frame
+        for resource_name, (amount, capacity) in camp_resources.items():
+            self.create_resource_frame(left_frame, resource_name, amount, capacity, f"Camp {camp_number}")
+        
+        ttk.Button(left_frame, text="Edit Camp", command=lambda cn=camp_number: self.show_screen(EditCamp, cn)).pack(pady=5)
+
+        # Create refugees section in right frame
+        self.create_refugees_section(right_frame)
+
+    def create_refugees_section(self, parent):
+        # Title for the refugees section
+        refugees_title = tk.Label(parent, text="Refugees", font=('Arial', 16, 'bold'))
+        refugees_title.pack(pady=10)
+
+        # Search and filter frame
+        search_frame = tk.Frame(parent, bg='white')
+        search_frame.pack(pady=5, fill='x')
+
+        # Dummy search bar
+        search_entry = tk.Entry(search_frame)
+        search_entry.pack(side='left', padx=5, expand=True, fill='x')
+
+        # Dummy filter button
+        filter_button = ttk.Button(search_frame, text="Filter")
+        filter_button.pack(side='left', padx=5)
+
+        # Dummy search button
+        search_button = ttk.Button(search_frame, text="Search")
+        search_button.pack(side='left', padx=5)
+
+        # Placeholder for refugee list
+        refugees_list_label = tk.Label(parent, text="Refugee List Placeholder", bg='white')
+        refugees_list_label.pack(pady=10, expand=True)
+
+        # Manage refugees button (dummy)
+        manage_refugees_button = ttk.Button(parent, text="Manage Refugees")
+        manage_refugees_button.pack(pady=5)
+
+    def create_resource_frame(self, parent, resource_name, amount, capacity, camp_name=None):
+        resource_frame = tk.Frame(parent)
+        resource_frame.pack(fill='x', expand=True)
+
+        tk.Label(resource_frame, text=f"{resource_name}:").pack(side='left')
+        amount_label = tk.Label(resource_frame, text=f"{amount}/{capacity}")
+        amount_label.pack(side='left')
+
+        ttk.Button(resource_frame, text="-", width=2, command=lambda: self.adjust_resource(camp_name, resource_name, -1, amount_label)).pack(side='left')
+        ttk.Button(resource_frame, text="+", width=2, command=lambda: self.adjust_resource(camp_name, resource_name, 1, amount_label)).pack(side='left')
+
+    def adjust_resource(self, adjustment):
+        new_amount = self.amount + adjustment
+        if 0 <= new_amount <= self.capacity:
+            self.amount = new_amount
+            self.amount_label.config(text=f"{self.amount}/{self.capacity}")
+            if self.adjust_callback:
+                self.adjust_callback(self.resource_name, new_amount)
+
+    def draw_barchart(self, canvas, data, x_offset, y_offset, bar_width, bar_spacing, max_bar_height, bar_color):
+        for i, (label, value) in enumerate(data):
+            x0 = x_offset + i * (bar_width + bar_spacing)
+            y1 = y_offset - (value / max_bar_height) * 200
+            canvas.create_rectangle(x0, y_offset, x0 + bar_width, y1, fill=bar_color)
+            canvas.create_text(x0 + (bar_width / 2), y_offset + 10, text=label, anchor='n')
 
 
 class AdminDashboard(Dashboard):
@@ -95,35 +131,23 @@ class AdminDashboard(Dashboard):
 
     def create_camp_tabs(self):
         self.tab_control = ttk.Notebook(self)
-
-        # Create an overview tab
         self.overview_tab = ttk.Frame(self.tab_control)
         self.tab_control.add(self.overview_tab, text='Overview')
         self.populate_overview_tab(self.overview_tab)
-
-        # Add tabs dynamically based on the number of camps
         for i in range(1, 4):
             tab = ttk.Frame(self.tab_control)
             self.tab_control.add(tab, text=f'Camp {i}')
             self.populate_camp_tab(tab, i)
-
         self.tab_control.pack(expand=1, fill="both")
 
 
 class VolunteerDashboard(Dashboard):
     def setup_dashboard(self):
-        self.create_volunteer_camp_tab()
+        self.create_camp_tabs()
 
-    def create_volunteer_camp_tab(self):
-        # Initialize a Notebook widget
+    def create_camp_tabs(self):
         self.tab_control = ttk.Notebook(self)
-
-        # Create a single tab for Camp 1
-        self.camp_tab = ttk.Frame(self.tab_control)
-        self.tab_control.add(self.camp_tab, text='Camp 1')
-
-        # Populate the camp tab with relevant widgets and data
-        self.populate_camp_tab(self.camp_tab, 1)
-
-        # Pack the Notebook widget
+        tab = ttk.Frame(self.tab_control)
+        self.tab_control.add(tab, text=f'Camp 1')
+        self.populate_camp_tab(tab,1)
         self.tab_control.pack(expand=1, fill="both")
