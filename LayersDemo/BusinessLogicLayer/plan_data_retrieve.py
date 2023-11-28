f33adf6168c0ab899efb78cdda54376ec93b891d
AnTienHuang <antien.huang@gmail.com>
2023-11-28 13:05:21 +0000

    update plan_data_retrieve

--- LayersDemo/BusinessLogicLayer/plan_data_retrieve.py
+++ LayersDemo/BusinessLogicLayer/plan_data_retrieve.py
@@ -4,7 +4,7 @@ from DataAccessLayer.data_access import DataAccess
 class PlanDataRetrieve:
 
     @staticmethod
-    def get_plan():
+    def get_plans():
         return DataAccess.get_all_plan()
 
     @staticmethod
@@ -19,7 +19,7 @@ class PlanDataRetrieve:
                     plan = DataAccess.get_plan_by_type(value)
                 case "region":
                     plan = DataAccess.get_plan_by_region(value)
-                case "plan id":
+                case "id":
                     plan = DataAccess.get_plan_by_plan_id(value)
                 case _:
                     return "You need to specify the filter name and attribute"
