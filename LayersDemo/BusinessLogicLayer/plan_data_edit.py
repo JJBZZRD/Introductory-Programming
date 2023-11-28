01cd89db7ba08b5fdc285f9a458b8256105137ce
kimvtxx <vichayasit.tantayapong@gmail.com>
2023-11-28 13:21:54 +0000

    update util and input

--- LayersDemo/BusinessLogicLayer/plan_data_edit.py
+++ LayersDemo/BusinessLogicLayer/plan_data_edit.py
@@ -1,6 +1,6 @@
 from DataAccessLayer.data_access import DataAccess
 from DataAccessLayer.data_access import UserDataAccess
-from BusinessLogicLayer.plan_data_validate import ValidatePlan
+import util
 from datetime import datetime
 import random
 
@@ -8,16 +8,7 @@ import random
 class PlanEdit:
 
     @staticmethod
-    def generate_plan_id():
-        num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        id_plan = ""
-        for i in range(6):
-            id_plan += str(random.choice(num))
-        return int(id_plan)
-
-    @staticmethod
-    def create_plan(name, start_date, plan_type, region, event, description):
-        # Validation
+    def create_plan(name, start_date, end_date, plan_type, region, event, description):
         for attr in [name, start_date, plan_type, region, event, description]:
             if not attr:
                 raise ValueError("Please provide {}".format(attr))
@@ -25,21 +16,21 @@ class PlanEdit:
             if not isinstance(attr, str):
                 raise ValueError("Please provide the correct input type for {}".format(attr))
 
-        start_date = ValidatePlan.validate_date(start_date, None)
-        plan_id = PlanEdit.generate_plan_id()
-        return UserDataAccess.create_plan(plan_id, name, start_date, plan_type, region, event, description)
+        start_date = util.validate_date(start_date, None)
+        return UserDataAccess.create_plan(name, start_date, end_date, plan_type, region, event, description)
 
     @staticmethod
-    def update_plan(plan_id, name=None, start_date=None, plan_type=None, region=None, event=None, description=None):
+    def update_plan(plan_id, name=None, start_date=None, end_date=None, plan_type=None, region=None, event=None, description=None):
         plan = DataAccess.get_plan_by_plan_id(plan_id)
 
         try:
-            name = ValidatePlan.validate_name(name)
-            start_date = ValidatePlan.validate_date(start_date, None)
-            plan_type = ValidatePlan.validate_plan_type(plan_type)
-            region = ValidatePlan.validate_region(region)
-            event = ValidatePlan.validate_event(event)
-            description = ValidatePlan.validate_description(description)
+            name = util.validate_name(name)
+            start_date = util.validate_date(start_date, None)
+            end_date = util.validate_date(start_date, end_date)
+            plan_type = util.validate_plan_type(plan_type)
+            region = util.validate_region(region)
+            event = util.validate_event(event)
+            description = util.validate_description(description)
         except ValueError:
             print("Invalid input")
 
@@ -58,17 +49,15 @@ class PlanEdit:
         return UserDataAccess.update_plan(plan_id, name, start_date, plan_type, region, event, description)
 
     @staticmethod
-    def end_plan(plan_id, end_date):
+    def end_plan(plan_id, start_date, end_date):
         plan = DataAccess.get_plan_by_plan_id(plan_id)
-        end_date = ValidatePlan.validate_date(None, end_date)
+        end_date = util.validate_date(start_date, end_date)
 
         if end_date:
             plan.end_date = end_date
 
             if datetime.today().date() == end_date:
-                return PlanEdit.delete_plan(plan_id)
-
-        return UserDataAccess.update_plan(plan_id, end_date)
+                return UserDataAccess.update_plan(plan_id, end_date)
 
     @staticmethod
     def delete_plan(plan_id):
--- LayersDemo/BusinessLogicLayer/plan_data_retrieve.py
+++ LayersDemo/BusinessLogicLayer/plan_data_retrieve.py
@@ -4,7 +4,7 @@ from DataAccessLayer.data_access import DataAccess
 class PlanDataRetrieve:
 
     @staticmethod
-    def get_plan():
+    def get_plans():
         return DataAccess.get_all_plan()
 
     @staticmethod
@@ -19,8 +19,12 @@ class PlanDataRetrieve:
                     plan = DataAccess.get_plan_by_type(value)
                 case "region":
                     plan = DataAccess.get_plan_by_region(value)
-                case "plan id":
+                case "id":
                     plan = DataAccess.get_plan_by_plan_id(value)
+                case "event":
+                    plan = DataAccess.get_plan_by_event_name(value)
+                case "description":
+                    plan = DataAccess.get_plan_by_description(value)
                 case _:
                     return "You need to specify the filter name and attribute"
         except:
