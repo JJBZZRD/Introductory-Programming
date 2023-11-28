d5b4cca4eb6cdece79108a716d35fe77f450f6ff
kimvtxx <vichayasit.tantayapong@gmail.com>
2023-11-28 12:57:16 +0000

    update plan_Data_retrieve

--- LayersDemo/BusinessLogicLayer/plan_data_retrieve.py
+++ LayersDemo/BusinessLogicLayer/plan_data_retrieve.py
@@ -4,18 +4,25 @@ from DataAccessLayer.data_access import DataAccess
 class PlanDataRetrieve:
 
     @staticmethod
-    def get_camp(filter, value):
+    def get_plan():
+        return DataAccess.get_all_plan()
+
+    @staticmethod
+    def get_plan(filter, value):
         plan = []
-        match filter:
-            case "name":
-                if value:
-                    plan = DataAccess.get_plan_by_name(value)
-            case "type":
-                plan = DataAccess.get_plan_by_type(value)
-            case "region":
-                plan = DataAccess.get_plan_by_region(value)
-            case "plan id":
-                plan = DataAccess.get_plan_by_plan_id(value)
-            case _:
-                return "You need to specify the filter name and attribute"
+        try:
+            match filter:
+                case "name":
+                    if value:
+                        plan = DataAccess.get_plan_by_name(value)
+                case "type":
+                    plan = DataAccess.get_plan_by_type(value)
+                case "region":
+                    plan = DataAccess.get_plan_by_region(value)
+                case "plan id":
+                    plan = DataAccess.get_plan_by_plan_id(value)
+                case _:
+                    return "You need to specify the filter name and attribute"
+        except:
+            return "Invalid inputs for get_plan(filter, value)"
         return plan
