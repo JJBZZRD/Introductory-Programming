ccf44420f1ddd7faf179564b0d08b13e449f0622
chizhang0213 <148653451+chizhang0213@users.noreply.github.com>
2023-12-07 15:58:29 +0000

    test the connection with database (camp)

--- DMS/Logic/camp_data_retrieve.py
+++ DMS/Logic/camp_data_retrieve.py
@@ -1,5 +1,10 @@
-from ..util import *
-from ..DB.camp import Camp
+import os
+import sys
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(r"path of config"))
+sys.path.append(CURRENT_DIR)
+from util import *
+
 
 class CampDataRetrieve:
 
@@ -9,36 +14,9 @@ class CampDataRetrieve:
         return parse_result('Camp', camp_tuples)
 
     @staticmethod
-    def get_camp(filter, value):
-        if not value:
-            return "You need to specify the filter name and value"
-        else:
-            try:
-                match filter:
-                    case "campID":
-                        if value:
-                            camp_tuples = Camp.get_camp(campID=value)
-                    case "location":
-                        camp_tuples = Camp.get_camp(location=value)
-                    # case "max_shelter":
-                    #     camp_infor = Camp.get_camp(max_shelter=attr)
-                    # case "water":
-                    #     camp_infor = Camp.get_camp(water=attr)
-                    # case "max_water":
-                    #     camp_infor = Camp.get_camp(max_water=attr)
-                    # case "food":
-                    #     camp_infor = Camp.get_camp(food=attr)
-                    # case "max_food":
-                    #     camp_infor = Camp.get_camp(max_food=attr)
-                    # case "medical_supplies":
-                    #     camp_infor = Camp.get_camp(medical_supplies=attr)
-                    # case "max_medical_supplies":
-                    #     camp_infor = Camp.get_camp(max_medical_supplies=attr)
-                    # case "planID":
-                    #     camp_infor = Camp.get_camp(planID=attr)
-                    case _:
-                        return "You need to specify the filter name and attribute"
-            except Exception as e:
-                return f"Error: {e}. Invalid inputs for get_camp(filter, value)"
-
-            return parse_result('Camp', camp_tuples)
+    def get_camp(campID=None, location=None, max_shelter=None, water=None, max_water=None, food=None, max_food=None,
+                 medical_supplies=None, max_medical_supplies=None, planID=None):
+        camp_tuples = Camp.get_camp(campID, location, max_shelter, water, max_water, food, max_food,
+                                    medical_supplies, max_medical_supplies, planID)
+        # add more validation procedure in the future
+        return parse_result('Camp', camp_tuples)
