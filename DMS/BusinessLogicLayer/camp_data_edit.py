5a6d7d337ac3d180e8a5c2b9db9d1128be542f21
chizhang0213 <148653451+chizhang0213@users.noreply.github.com>
2023-12-07 15:54:52 +0000

    test the connection with database

--- DMS/BusinessLogicLayer/camp_data_edit.py
+++ DMS/BusinessLogicLayer/camp_data_edit.py
@@ -1,7 +1,7 @@
-from ..DataLayer.camp import Camp
-from ..DataLayer.plan import Plan
-from .camp_data_retrieve import CampDataRetrieve
-from .. import util
+from DataLayer.camp import Camp
+from DataLayer.plan import Plan
+from camp_data_retrieve import CampDataRetrieve
+import util
 
 
 class CampDataEdit:
@@ -10,11 +10,11 @@ class CampDataEdit:
     def update_camp(campID, location=None, max_shelter=None, water=None, max_water=None, food=None, max_food=None,
                     medical_supplies=None, max_medical_supplies=None, planID=None):
         # get variables
-        camp = CampDataRetrieve.get_camp('campID', campID)
-
+        camp = CampDataRetrieve.get_camp(campID=campID)
+        camp = camp[0]
         try:
             if location:
-                camp.location = location.strip()
+                camp = location
             if max_shelter:
                 camp.max_shelter = max_shelter
             if water:
@@ -34,67 +34,79 @@ class CampDataEdit:
         except:
             return "Invalid inputs, please check and try again"
         # validate
-        if not util.is_country(location):
-            return "You should enter a country name for real."
+        if location:
+            if not util.is_country(location):
+                return "You should enter a country name for real."
 
-        if util.is_num(max_shelter):
-            if not util.is_positive(max_shelter):
-                return "Cannot enter a negative value to max_shelter."
-        else:
-            return "You should enter a number to max_shelter."
+        if max_shelter:
+            if util.is_num(max_shelter):
+                if not util.is_positive(max_shelter):
+                    return "Cannot enter a negative value to max_shelter."
+            else:
+                return "You should enter a number to max_shelter."
 
-        if util.is_num(water):
-            if not util.is_positive(water):
-                return "Cannot enter a negative value to water level."
-        else:
-            return "You should enter a number to water level."
+        if water:
+            if util.is_num(water):
+                if not util.is_positive(water):
+                    return "Cannot enter a negative value to water level."
+            else:
+                return "You should enter a number to water level."
 
-        if util.is_num(max_water):
-            if not util.is_positive(max_water):
-                return "Cannot enter a negative value to max_water."
-        else:
-            return "You should enter a number to max_water."
+        if max_water:
+            if util.is_num(max_water):
+                if not util.is_positive(max_water):
+                    return "Cannot enter a negative value to max_water."
+            else:
+                return "You should enter a number to max_water."
 
-        if util.is_num(food):
-            if not util.is_positive(food):
-                return "Cannot enter a negative value to food level."
-        else:
-            return "You should enter a number to food level."
+        if food:
+            if util.is_num(food):
+                if not util.is_positive(food):
+                    return "Cannot enter a negative value to food level."
+            else:
+                return "You should enter a number to food level."
 
-        if util.is_num(max_food):
-            if not util.is_positive(max_food):
-                return "Cannot enter a negative value to max_food."
-        else:
-            return "You should enter a number to max_food."
+        if max_food:
+            if util.is_num(max_food):
+                if not util.is_positive(max_food):
+                    return "Cannot enter a negative value to max_food."
+            else:
+                return "You should enter a number to max_food."
 
-        if util.is_num(medical_supplies):
-            if not util.is_positive(medical_supplies):
-                return "Cannot enter a negative value to medical_supplies."
-        else:
-            return "You should enter a number to medical_supplies."
+        if medical_supplies:
+            if util.is_num(medical_supplies):
+                if not util.is_positive(medical_supplies):
+                    return "Cannot enter a negative value to medical_supplies."
+            else:
+                return "You should enter a number to medical_supplies."
 
-        if util.is_num(max_medical_supplies):
-            if not util.is_positive(max_medical_supplies):
-                return "Cannot enter a negative value to max_medical_supplies."
-        else:
-            return "You should enter a number to max_medical_supplies."
+        if max_medical_supplies:
+            if util.is_num(max_medical_supplies):
+                if not util.is_positive(max_medical_supplies):
+                    return "Cannot enter a negative value to max_medical_supplies."
+            else:
+                return "You should enter a number to max_medical_supplies."
 
-        if util.is_num(planID):
-            if not Plan.get_planID(planID):
-                return "Cannot find this planID. Please try again."
-        else:
-            return "You should enter a number to planID."
+        if planID:
+            if util.is_num(planID):
+                if not Plan.get_planID(planID):
+                    return "Cannot find this planID. Please try again."
+            else:
+                return "You should enter a number to planID."
 
-        camp_tuples = Camp.update_camp(campID, location, max_shelter, water, max_water, food,
-                                       max_food, medical_supplies, max_medical_supplies, planID)
+        camp_tuple = [Camp.update_camp(campID, location, max_shelter, water, max_water, food,
+                                       max_food, medical_supplies, max_medical_supplies, planID)]
 
-        return util.parse_result('Camp', camp_tuples)
+        return util.parse_result('Camp', camp_tuple)
 
     @staticmethod  # Insert a camp into the database without creating a new instance
-    def create_camp(campID, location, max_shelter, water, max_water, food, max_food, medical_supplies,
+    def create_camp(location, max_shelter, water, max_water, food, max_food, medical_supplies,
                     max_medical_supplies, planID):
-        return Camp.create_camp(location, max_shelter, water, max_water, food, max_food, medical_supplies,
-                                max_medical_supplies, planID)
+        camp = (location, max_shelter, water, max_water, food, max_food, medical_supplies,
+                max_medical_supplies, planID)
+        campID = Camp.create_camp(camp_tuple=camp)
+        camp_tuple = [Camp.get_campID(campID)]
+        return util.parse_result('Camp', camp_tuple)
 
     @staticmethod
     def delete_camp(campID):
--- DMS/BusinessLogicLayer/test_camp.py
+++ DMS/BusinessLogicLayer/test_camp.py
@@ -1,24 +1,77 @@
+import os
+import sys
+
+CURRENT_DIR = os.path.dirname(
+    os.path.abspath(r"C:\Users\10927\PycharmProjects\pythonProject\COMP0066\DataLayer\config.py"))
+sys.path.append(CURRENT_DIR)
+import unittest
+import sqlite3
+from camp_data_retrieve import CampDataRetrieve
 from camp_data_edit import CampDataEdit
-from ..DataLayer.camp import Camp
-from ..DataLayer.plan import Plan
-from .camp_data_retrieve import CampDataRetrieve
-from .. import util
+from DataLayer.plan import Plan
+from DataLayer.camp import Camp
+from util import *
+from DataLayer.config import *
 
 
-new_plan = Plan.create_plan('2023-12-04', None, 'test Plan', 'test region', 'test event name', 'aaaaaa')
+class TestCamp(unittest.TestCase):
 
-planID = new_plan[0]
-print(new_plan)
+    def setUp(self):
+        # Set up a temporary SQLite database for testing
+        print("Setting up resources for the test")
+        self.connection = sqlite3.connect(':memory:')
+        self.cursor = self.connection.cursor()
+        create_database()
 
-a = CampDataEdit.create_camp(0, 'Australia', 50, None, 50, None, 50, None, 50, planID)
-print(a)
+    def tearDown(self) -> None:
+        print("Tearing down resources after the test")
+        self.connection.close()
 
-b = CampDataRetrieve.get_all_camps()
-print(Camp.get_campID(71))
+    def test_create_camp(self):
+        print("\nExecuting test_create_camp")
 
-c = CampDataRetrieve.get_camp("campID", 51)
-print(c)
+        camp_a = CampDataEdit.create_camp('a', 50, None, 50, None, 50, None, 50, 10)
+
+        self.assertIsInstance(camp_a, list, "camp_a is not a list")
+
+        pass
+
+    def test_update_camp(self):
+        print("\nExecuting update_camp")
+
+        camp_b = CampDataEdit.update_camp(1, location='Australia')
+
+        self.assertIsInstance(camp_b, list, "camp_b is not a list")
+
+        pass
+
+    # def test_get_all_camp(self):
+    #     print("\nExecuting get_all_camps")
+    #
+    #     camp_c = CampDataRetrieve.get_all_camps()
+    #
+    #     self.assertIsInstance(camp_c, list, "camp_c is not a list")
+    #
+    #     pass
+
+    def test_get_camp(self):
+        print("\nExecuting get_camp")
+
+        camp_d = CampDataRetrieve.get_camp(campID=1)
+
+        self.assertIsInstance(camp_d, list, "camp_d is not a list")
+
+        pass
+
+    def test_get_camp_blank(self):
+        print("\nExecuting get_camp with blank")
+
+        camp_e = CampDataRetrieve.get_camp(campID=1, location='China')
+
+        self.assertIsInstance(camp_e, list, "camp_e is not a list")
+
+        pass
 
 
-
-
+if __name__ == '__main__':
+    unittest.main()
