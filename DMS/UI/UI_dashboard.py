cc31e54a661d479d96dd318a23a39b6edf2527a1
lukejlatham <141853040+lukejlatham@users.noreply.github.com>
2023-12-07 13:46:16 +0000

    dog :wq

--- DMS/UI/UI_dashboard.py
+++ DMS/UI/UI_dashboard.py
@@ -159,7 +159,7 @@ class AdminDashboard(Dashboard):
     """
     
     def setup_dashboard(self, plan):
-        self.planCamps = PlanDataRetrieve.get_camps(plan.planID)
+        self.planCamps = CampDataRetrieve.get_camp('planID',plan.planID)[0]
         self.additional_resources = {'Food': [40, 100], 'Water': [30, 80], 'Medicine': [10, 60], 'Shelter': [10,90]} 
         self.create_admin_tabs(self.planCamps, self.additional_resources)
     
