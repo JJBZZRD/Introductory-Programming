f2dedf8989452232e8d0505ff81b43cd1bc7d5b4
AnTienHuang <antien.huang@gmail.com>
2023-12-12 23:45:24 +0000

    fix create camp

--- DMS/Logic/camp_data_edit.py
+++ DMS/Logic/camp_data_edit.py
@@ -59,7 +59,7 @@ class CampDataEdit:
                 return "You should enter a number to medical_supplies."
         if planID:
             if util.is_num(planID):
-                if not Plan.get_planID(planID):
+                if len(Plan.get_plan_by_id(planID)) < 1:
                     return "Cannot find this planID. Please try again."
 
         camp_tuple = Camp.update_camp(campID, location, shelter, water, food,medical_supplies, planID)
