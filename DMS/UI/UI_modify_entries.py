0763d961ddcfbc3c009f88e949d348c704e055bf
JJBZZRD <buzzard.jj@gmail.com>
2023-12-13 15:17:13 +0000

    fixed the entry fields issue

--- DMS/UI/UI_modify_entries.py
+++ DMS/UI/UI_modify_entries.py
@@ -61,40 +61,38 @@ class ModifyEntries(tk.Frame):
         k = 1
         j = 0
         for i, variable in enumerate(self.modifiable_variables):  # this loop creates a vertical list of columns that is 4 high maximum
-            if variable is not None:
-                variable_in_passed_id = False if self.passed_id is None else variable in self.passed_id
-                if variable in self.fields_to_be_dropdown and not variable_in_passed_id:
-                    entry_name = ttk.Label(self.lower_frame, text=variable)
-                    entry_name.grid(column=k, row=j, pady=5, padx=5)
+            if variable in self.fields_to_be_dropdown and variable not in self.passed_id:
+                entry_name = ttk.Label(self.lower_frame, text=variable)
+                entry_name.grid(column=k, row=j, pady=5, padx=5)
 
-                    drop_down = ttk.Combobox(self.lower_frame, values=self.fields_to_be_dropdown[variable], state='readonly')
+                drop_down = ttk.Combobox(self.lower_frame, values=self.fields_to_be_dropdown[variable], state='readonly')
 
-                    if self.current_data is not None and i < len(self.current_data):
-                        drop_down.set(self.current_data[i])
-                    else:
-                        drop_down.set(self.fields_to_be_dropdown[variable][0])
-                    drop_down.grid(column=k + 1, row=j, pady=5, padx=5)
-                    self.entry_fields.update({variable: [self.filter_matching[variable], drop_down]})
+                if self.current_data is not None and i < len(self.current_data):
+                    drop_down.set(self.current_data[i])
                 else:
-                    entry_name = ttk.Label(self.lower_frame, text=variable)
-                    entry_name.grid(column=k, row=j, pady=5, padx=5)
+                    drop_down.set(self.fields_to_be_dropdown[variable][0])
+                drop_down.grid(column=k + 1, row=j, pady=5, padx=5)
+                self.entry_fields.update({variable: [self.filter_matching[variable], drop_down]})
+            else:
+                entry_name = ttk.Label(self.lower_frame, text=variable)
+                entry_name.grid(column=k, row=j, pady=5, padx=5)
 
-                    entry_field = ttk.Entry(self.lower_frame)
-                    entry_field.grid(column=k + 1, row=j, pady=5, padx=5)
+                entry_field = ttk.Entry(self.lower_frame)
+                entry_field.grid(column=k + 1, row=j, pady=5, padx=5)
 
-                    self.entry_fields.update({variable: [self.filter_matching[variable], entry_field]})
-                    
-                    if variable_in_passed_id:
-                        placeholder = self.passed_id[variable]
+                self.entry_fields.update({variable: [self.filter_matching[variable], entry_field]})
+                
+                if variable in self.passed_id:
+                    placeholder = self.passed_id[variable]
 
-                    elif self.current_data is not None and i < len(self.current_data):
-                        placeholder = self.current_data[i]
-                    elif "Date" in variable:
-                        placeholder = "yyyy-mm-dd"
-                    elif variable in ["Water", "Food", "Medical Supplies", "Shelter"]:
-                        placeholder = "0"
-                    else:
-                        placeholder = "Enter " + variable
+                elif self.current_data is not None and i < len(self.current_data):
+                    placeholder = self.current_data[i]
+                elif "Date" in variable:
+                    placeholder = "yyyy-mm-dd"
+                elif variable in ["Water", "Food", "Medical Supplies", "Shelter"]:
+                    placeholder = "0"
+                else:
+                    placeholder = "Enter " + variable
 
 
                 entry_field.insert(0, placeholder)
@@ -258,9 +256,9 @@ class EditPlan(ModifyEntries):
     def setup_modify(self):
         self.lower_frame = tk.Frame(self)
         self.modify_type = ['View/Edit Plan']
-        self.modifiable_variables = ['Plan ID', 'Plan Name', 'Country', 'Event Name', 'Description', 'Start Date', 'End Date', 'Water', 'Food', 'Medical Supplies', 'Shelter', 'Status']
+        self.modifiable_variables = ['Plan ID', 'Plan Name', 'Country', 'Event Name', 'Description', 'Start Date', 'End Date', 'Water', 'Food', 'Medical Supplies', 'Shelter', 'Status', 'Creation Time']
         self.filter_matching = {'Plan ID': 'planID', 'Plan Name': 'name', 'Country': 'country', 'Event Name': 'event_name',
-                                'Description': 'description', 'Start Date': 'start_date', 'End Date': 'end_date', 'Water': 'water', 'Food': 'food', 'Medical Supplies': 'medical_supplies', 'Shelter': 'shelter', 'Status':'status'}
+                                'Description': 'description', 'Start Date': 'start_date', 'End Date': 'end_date', 'Water': 'water', 'Food': 'food', 'Medical Supplies': 'medical_supplies', 'Shelter': 'shelter', 'Status':'status', 'Creation Time': 'created_time'}
         self.button_labels = ['Save Changes', 'Delete']
         self.current_data = self.screen_data.display_info()
         self.save_record = PlanEdit.update_plan
@@ -270,7 +268,7 @@ class EditPlan(ModifyEntries):
         self.screen_data_id = self.screen_data.planID
         self.entry_fields = {}
         self.fields_to_be_dropdown = {'Country': get_all_countries(), 'Status': ['Active', 'Ended']}
-        self.read_only_fields = ['Plan ID']
+        self.read_only_fields = ['Plan ID', 'Creation Time']
         self.create_title()
         self.create_entry_fields()
         self.create_buttons()
@@ -302,7 +300,7 @@ class NewCamp(ModifyEntries):
 
             return {'Plan ID':planID}
         else:
-            return None
+            return {}
 
 
 class EditCamp(ModifyEntries):
