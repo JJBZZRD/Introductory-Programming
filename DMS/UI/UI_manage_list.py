7d42059514a03de5285a62864db244fcda43c1a2
JJBZZRD <buzzard.jj@gmail.com>
2023-12-13 18:53:20 +0000

    fixed activity status filter for volunteers

--- DMS/UI/UI_manage_list.py
+++ DMS/UI/UI_manage_list.py
@@ -72,7 +72,7 @@ class ManageList(tk.Frame):
                 case 'Ended':
                     get_list_input.update({'active': False})
                 
-                case 'inactive':
+                case 'Inactive':
                     get_list_input.update({'active': False})
                 
 
@@ -269,14 +269,14 @@ class VolunteerList(ManageList):
         self.list_headers = ['Volunteer ID', 'First Name', 'Last Name', 'Username',
                 'Date of Birth', 'Phone', 'Account Status', 'Camp ID', 'Creation Time']
         self.filter_values = ['Volunteer ID', 'First Name', 'Last Name', 'Username',
-                'Date of Birth', 'Phone', 'Account Status', 'Camp ID']
+                'Date of Birth', 'Phone', 'Camp ID']
         self.list_data = self.list_by_plan()
         self.get_search = PersonDataRetrieve.get_volunteers
         self.switch_to_page = 'EditVolunteer'
         self.record_button = 'NewVolunteer'
         self.filter_matching = {'Volunteer ID': 'volunteerID', 'First Name': 'name', 'Last Name': 'name', 'Username': 'username',
                 'Date of Birth': 'date_of_birth', 'Phone': 'phone', 'Camp ID': 'campID', 'Creation Time': 'created_time'}
-        self.status_filters = ['All', 'Active', 'Deactivated']
+        self.status_filters = ['All', 'Active', 'Inactive']
         self.modify_title()
         self.create_title()
         self.create_search()
