1f1a1224422c7e090a281c601fa71a8fcb99c241
AnTienHuang <antien.huang@gmail.com>
2023-12-13 16:08:05 +0000

    update end_plan

--- DMS/Logic/plan_data_edit.py
+++ DMS/Logic/plan_data_edit.py
@@ -85,6 +85,8 @@ class PlanEdit:
         if status and status not in ['Active', 'Ended']:
             return "Invalid status"
 
+        #return message to plan ui
+
         res = Plan.update_plan(planID, start_date, end_date, name, country, event_name, description, water, food, shelter, medical_supplies, status)
 
         print(f"res:  {res}")
