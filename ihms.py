6b6c8d6216baaac0f96d23f27b0710774eb7d197
AnTienHuang <antien.huang@gmail.com>
2023-12-15 00:43:36 +0000

    update ihms.py for linux

--- ihms.py
+++ ihms.py
@@ -1,24 +1,43 @@
 import subprocess
 import platform
 
-try:
-    subprocess.run(["pip", "install", "numpy", "pandas"])
-
-    if platform.system() == "Windows":
-        subprocess.run(["pip", "install", "tk"])
-    elif platform.system() == "Darwin":
-        subprocess.run(["pip", "install", "tk"])
-    elif platform.system() == "Linux":
-        subprocess.run(["sudo", "apt-get", "install", "-y", "python3-tk"])
-
-    subprocess.run(["python", "temp.py"])
-except:
-    subprocess.run(["pip3", "install", "numpy", "pandas"])
-    if platform.system() == "Windows":
-        subprocess.run(["pip3", "install", "tk"])
-    elif platform.system() == "Darwin":
-        subprocess.run(["pip3", "install", "tk"])
-    elif platform.system() == "Linux":
-        subprocess.run(["sudo", "apt-get", "install", "-y", "python3-tk"])
-
-    subprocess.run(["python3", "temp.py"]) 
+if platform.system() in ["Windows", "Darwin"]:
+    try:
+        subprocess.run(["pip", "install", "numpy", "pandas", "tk"])
+    except:
+        subprocess.run(["pip3", "install", "numpy", "pandas", "tk"])
+    finally:
+        try:
+            subprocess.run(["python3", "temp.py"]) 
+        except:
+            subprocess.run(["python", "temp.py"])
+elif platform.system() == "Linux":
+    try:
+        subprocess.run(["sudo", "apt-get", "install", "python3-pandas"])
+    except:
+        subprocess.run(["sudo", "pip", "install", "pandas"])
+    try:
+        subprocess.run(["sudo", "pip", "install", "numpy"])
+    except:
+        subprocess.run(["sudo", "apt-get", "install", "python-pip"])
+        subprocess.run(["sudo", "pip", "install", "numpy"])
+    try:
+        subprocess.run(["sudo", "apt-get", "install", "python3-tk"])
+    except:
+        subprocess.run(["sudo", "apt-get", "install", "python-pip"])
+        subprocess.run(["sudo", "pip", "install", "tk"])
+    finally:
+        try:
+            subprocess.run(["python3", "temp.py"])
+        except:   
+            subprocess.run(["python", "temp.py"])
+else:
+    try:
+        subprocess.run(["pip", "install", "numpy", "pandas", "tk"])
+    except:
+        subprocess.run(["pip3", "install", "numpy", "pandas", "tk"])
+    finally:
+        try:
+            subprocess.run(["python3", "temp.py"]) 
+        except:
+            subprocess.run(["python", "temp.py"])
